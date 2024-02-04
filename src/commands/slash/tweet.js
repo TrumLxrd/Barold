167a5cb77d416c2cf3476d52e480594a2e6a8b3f
TrumLxrd <73558403+TrumLxrd@users.noreply.github.com>
2024-02-04 17:59:12 +0200

    Update tweet.js

--- src/commands/slash/tweet.js
+++ src/commands/slash/tweet.js
@@ -1,6 +1,7 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
 const Jimp = require("jimp");
 const { unlink } = require("fs");
+const { EmbedBuilder } = require("discord.js");
 
 module.exports = {
   data: new SlashCommandBuilder()
